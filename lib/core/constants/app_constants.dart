edb55a9da570eada694783db980ab298989838cd
NichWes <nicholaswestley06@gmail.com>
2026-05-11 18:51:45 +0700

    membuat branch midtrans review flutter dan revisi baseUrl

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 class AppConstants {
-  static const String baseUrl = 'https://velinciahpl.com/api';
+  static const String baseUrl = 'https:/review.velinciahpl.com/api';
 
   static const String pusherKey = 'cb6d4c35a4a69e0e5d20';
   static const String pusherCluster = 'ap1';
